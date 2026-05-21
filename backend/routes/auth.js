cc0e4abaeb1e9adbe43c92dda1eff173c071a390
AARYAN JINDAL <aaryanjindal15@gmail.com>
2026-05-22 00:41:52 +0530

    New vercel URL

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -106,12 +106,12 @@ router.get("/google",(req,res,next)=>{
 );
 router.get("/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "https://vibecraft-ochre.vercel.app/error"
+    failureRedirect: "https://vibecraftt-ten.vercel.app/error"
   }),
   (req, res) => {
     const redirect = req.session.redirectAfterLogin || "/ai";
     req.session.redirectAfterLogin = null;  
-    res.redirect(`https://vibecraft-ochre.vercel.app${redirect}`);
+    res.redirect(`https://vibecraftt-ten.vercel.app${redirect}`);
   }
 );
 module.exports = router;
